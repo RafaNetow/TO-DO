a13cd76cedf08d8fc48673c51921d3ecb5936a40
Rafael Sequeiros <rsequeiros@supports-MacBook-Pro.local>
2020-03-05 16:00:54 -0600

    added css

---
+++ todo-react/src/components/todo-item/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class todoItem extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return <div> </div>
+  }
+}
